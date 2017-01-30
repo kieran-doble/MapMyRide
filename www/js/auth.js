8c235bd994b3c0a3cc75cf28b32a79bd31981476
Kieran Doble <kieran.doble@gmail.com>
2017-01-30 17:39:18 +0000

    testing

--- www/js/auth.js
+++ www/js/auth.js
@@ -40,32 +40,34 @@ $(document).ready(function(){
     	var email=$("#email").val();
     	var password=$("#password").val();
     	var dataString="fullname="+fullname+"&email="+email+"&password="+password+"&signup=";
-
-    	if($.trim(fullname).length>0 && $.trim(email).length>0 && $.trim(password).length>0)
-		{
-			$.ajax({
-				type: "POST",
-				url: url,
-				data: dataString,
-				crossDomain: true,
-				cache: false,
-				beforeSend: function(){ $("#signup").val('Connecting...');},
-				success: function(data){
-					if(data === "success")
-					{
-						alert("Thank you for Registering with us! you can login now");
-					}
-					else if(data === "exist")
-					{
-						alert("Hey! You alreay has account! you can login with us");
-					}
-					else if(data === "failed")
-					{
-						alert("Something Went wrong");
-					}
-				}
-			});
-		}return false;
+			console.log($.trim(fullname));
+			console.log($.trim(email));
+			console.log($.trim (password));
+    // 	if($.trim(fullname).length>0 && $.trim(email).length>0 && $.trim(password).length>0)
+		// {
+		// 	$.ajax({
+		// 		type: "POST",
+		// 		url: url,
+		// 		data: dataString,
+		// 		crossDomain: true,
+		// 		cache: false,
+		// 		beforeSend: function(){ $("#signup").val('Connecting...');},
+		// 		success: function(data){
+		// 			if(data === "success")
+		// 			{
+		// 				alert("Thank you for Registering with us! you can login now");
+		// 			}
+		// 			else if(data === "exist")
+		// 			{
+		// 				alert("Hey! You alreay has account! you can login with us");
+		// 			}
+		// 			else if(data === "failed")
+		// 			{
+		// 				alert("Something Went wrong");
+		// 			}
+		// 		}
+		// 	});
+		// }return false;
 
     });
 
