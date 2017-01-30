ed8399c8bdda42d7d60c7fe705bdb437be184e1e
Kieran Doble <kieran.doble@gmail.com>
2017-01-30 17:10:40 +0000

    final?

--- Server/auth.php
+++ Server/auth.php
@@ -43,13 +43,13 @@ if(isset($_POST['login']))
 {
 	$email=mysqli_real_escape_string($db, $_POST['email']);
 	$password=mysqli_real_escape_string($db, $_POST['password']);
-	$login=mysqli_query($db, "select * from 'phonegap_login' where `email`='{$email}' and `password`='{$password}'");
-	print_r ($login);
-	// if (mysqli_num_rows($login)) {
-	//     echo "success";
-	// } else {
-	//     echo "failed";
-	// }
+	$login=mysqli_query($db, "select * from phonegap_login where `email`='{$email}' and `password`='{$password}'");
+
+	if (mysqli_num_rows($login)) {
+	    echo "success";
+	} else {
+	    echo "failed";
+	}
 }
 
 //Change Password
