f96b9ec478799ab0bf566a034277f40ebb3a9cb5
Kieran Doble <kieran.doble@gmail.com>
2017-01-30 17:32:37 +0000

    testing

--- platforms/ios/www/js/auth.js
+++ platforms/ios/www/js/auth.js
@@ -36,37 +36,37 @@ $(document).ready(function(){
 
     //signup function
     $("#signup").click(function(){
-    	var fullname=$("#fullname").val();
-    	var email=$("#email").val();
-    	var password=$("#password").val();
-    	var dataString="fullname="+fullname+"&email="+email+"&password="+password+"&signup=";
-
-    	if($.trim(fullname).length>0 & $.trim(email).length>0 & $.trim(password).length>0)
-		{
-			$.ajax({
-				type: "POST",
-				url: url,
-				data: dataString,
-				crossDomain: true,
-				cache: false,
-				beforeSend: function(){ $("#signup").val('Connecting...');},
-				success: function(data){
-					if(data === "success")
-					{
-						alert("Thank you for Registering with us! you can login now");
-					}
-					else if(data === "exist")
-					{
-						alert("Hey! You alreay has account! you can login with us");
-					}
-					else if(data === "failed")
-					{
-						alert("Something Went wrong");
-					}
-				}
-			});
-		}return false;
-
+    // 	var fullname=$("#fullname").val();
+    // 	var email=$("#email").val();
+    // 	var password=$("#password").val();
+    // 	var dataString="fullname="+fullname+"&email="+email+"&password="+password+"&signup=";
+		//
+    // 	if($.trim(fullname).length>0 & $.trim(email).length>0 & $.trim(password).length>0)
+		// {
+		// 	$.ajax({
+		// 		type: "POST",
+		// 		url: url,
+		// 		data: dataString,
+		// 		crossDomain: true,
+		// 		cache: false,
+		// 		beforeSend: function(){ $("#signup").val('Connecting...');},
+		// 		success: function(data){
+		// 			if(data === "success")
+		// 			{
+		// 				alert("Thank you for Registering with us! you can login now");
+		// 			}
+		// 			else if(data === "exist")
+		// 			{
+		// 				alert("Hey! You alreay has account! you can login with us");
+		// 			}
+		// 			else if(data === "failed")
+		// 			{
+		// 				alert("Something Went wrong");
+		// 			}
+		// 		}
+		// 	});
+		// }return false;
+		console.log('hello');
     });
 
     //Change Password
