9979be2fb27a3b092bd2ab6278ea3c052815fbbf
Kieran Doble <kieran.doble@gmail.com>
2017-01-30 12:33:40 +0000

    auth.php edits based on comments in git

--- Server/auth.php
+++ Server/auth.php
@@ -12,18 +12,18 @@ if (mysqli_connect_errno()) {
 //Create New Account
 if(isset($_POST['signup']))
 {
-	$fullname=$_POST['fullname'];
-	$email=$_POST['email'];
-	$password=$_POST['password'];
+	$fullname=mysql_real_escape_string($_POST['fullname']);
+	$email=mysql_real_escape_string($_POST['email']);
+	$password=mysql_real_escape_string($_POST['password']);
 	$login = mysqli_query($db, "SELECT * FROM phonegap_login WHERE email = '$email'");
-	if($login!=0)
+	if (mysqli_num_rows($login)) {
+		echo "Account exists";
+} else {
 	{
-		echo "exist";
-	}
-	else
-	{
-		$newAccount=mysqli_query($db, "INSERT INTO phonegap_login (fullname`,`email`,`password`) values ('$fullname','$email','$password')");
-		if($newAccount)
+		// account does not exist
+		$newAccount=mysqli_query($db, "INSERT INTO phonegap_login (fullname`,`email`,`password`)
+			values ('$fullname','$email', SHA2('$password'))");
+		if (mysqli_num_rows($newAccount))
 		{
 			echo "success";
 		}
@@ -38,16 +38,13 @@ if(isset($_POST['signup']))
 //Login
 if(isset($_POST['login']))
 {
-	$email=$_POST['email'];
-	$password=$_POST['password'];
-	$login=mysqli_query("select * from `phonegap_login` where `email`='$email' and `password`='$password'");
-	if($login!=0)
-	{
-		echo "success";
-	}
-	else
-	{
-		echo "failed";
+	$email=mysql_real_escape_string($_POST['email']);
+	$password=mysql_real_escape_string($_POST['password']);
+	$login=mysqli_query($db, "select * from `phonegap_login` where `email`='$email' and `password`='$password'");
+	if (mysqli_num_rows($login)) {
+	    echo "success";
+	} else {
+	    echo "failed";
 	}
 }
 
@@ -55,12 +52,12 @@ if(isset($_POST['login']))
 if(isset($_POST['change_password']))
 {
 	$email=$_POST['email'];
-	$old_password=$_POST['old_password'];
-	$new_password=$_POST['new_password'];
-	$check=mysql_query("select * from `phonegap_login` where `email`='$email' and `password`='$old_password'");
-	if($check!=0)
+	$old_password=mysql_real_escape_string($_POST['old_password']);
+	$new_password=mysql_real_escape_string($_POST['new_password']);
+	$check=mysqli_query($db, "select * from `phonegap_login` where `email`='$email' and `password`='$old_password'");
+	if(mysqli_num_rows($check))
 	{
-		mysql_query("update `phonegap_login` set `password`='$new_password' where `email`='$email'");
+		mysqli_query($db "update `phonegap_login` set `password`='$new_password' where `email`='$email'");
 		echo "success";
 	}
 	else
@@ -73,9 +70,9 @@ if(isset($_POST['change_password']))
 if(isset($_POST['forget_password']))
 {
 	$email=$_POST['email'];
-	$q=mysqli_query("select * from `phonegap_login` where `email`='$email'");
-	$check=mysql_num_rows($q);
-	if($check!=0)
+	$q=mysqli_query($db, "select * from `phonegap_login` where `email`='$email'");
+	$check=mysqli_num_rows($q);
+	if(mysqli_num_rows($check))
 	{
 		echo "success";
 		$data=mysql_fetch_array($q);
