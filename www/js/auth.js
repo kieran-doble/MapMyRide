a9121ce13617a5acd7add80add6771d0fd33b333
Kieran Doble <kieran.doble@gmail.com>
2017-01-30 17:48:16 +0000

    testing!

--- www/js/auth.js
+++ www/js/auth.js
@@ -42,29 +42,30 @@ $(document).ready(function(){
     	var dataString="fullname="+fullname+"&email="+email+"&password="+password+"&signup=";
 
     	if($.trim(fullname).length > 0 && $.trim(email).length > 0 && $.trim(password).length > 0){
-				console.log('boop');
-			// $.ajax({
-			// 	type: "POST",
-			// 	url: url,
-			// 	data: dataString,
-			// 	crossDomain: true,
-			// 	cache: false,
-			// 	beforeSend: function(){ $("#signup").val('Connecting...');},
-			// 	success: function(data){
-			// 		if(data === "success")
-			// 		{
-			// 			alert("Thank you for Registering with us! you can login now");
-			// 		}
-			// 		else if(data === "exist")
-			// 		{
-			// 			alert("Hey! You alreay has account! you can login with us");
-			// 		}
-			// 		else if(data === "failed")
-			// 		{
-			// 			alert("Something Went wrong");
-			// 		}
-			// 	}
-			// });
+			$.ajax({
+				type: "POST",
+				url: url,
+				data: dataString,
+				crossDomain: true,
+				cache: false,
+				beforeSend: function(){ $("#signup").val('Connecting...');},
+				success: function(data){
+					if(data === "success"){
+						alert("Thank you for Registering with us! you can login now");
+					}
+					else if(data === "exist"){
+						alert("Hey! You alreay has account! you can login with us");
+					}
+					else if(data === "failed"){
+						alert("Something Went wrong");
+					}
+				},
+				error: function(err){
+					if(err){
+						console.log(err);
+					}
+				}
+			});
 		}
 		return false;
 
