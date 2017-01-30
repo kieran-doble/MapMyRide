618e752a5d8ee6140439f4ab000d222b3d7f0771
Kieran Doble <kieran.doble@gmail.com>
2017-01-30 17:51:21 +0000

    removed testing

--- www/js/auth.js
+++ www/js/auth.js
@@ -42,6 +42,7 @@ $(document).ready(function(){
     	var dataString="fullname="+fullname+"&email="+email+"&password="+password+"&signup=";
 
     	if($.trim(fullname).length > 0 && $.trim(email).length > 0 && $.trim(password).length > 0){
+
 			$.ajax({
 				type: "POST",
 				url: url,
@@ -50,19 +51,17 @@ $(document).ready(function(){
 				cache: false,
 				beforeSend: function(){ $("#signup").val('Connecting...');},
 				success: function(data){
-					if(data === "success"){
+					if(data === "success")
+					{
 						alert("Thank you for Registering with us! you can login now");
 					}
-					else if(data === "exist"){
+					else if(data === "exist")
+					{
 						alert("Hey! You alreay has account! you can login with us");
 					}
-					else if(data === "failed"){
+					else if(data === "failed")
+					{
 						alert("Something Went wrong");
-					}
-				},
-				error: function(err){
-					if(err){
-						console.log(err);
 					}
 				}
 			});
